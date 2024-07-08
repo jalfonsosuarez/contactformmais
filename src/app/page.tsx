12c77e772623e15687d3cb68d6b2b85e700e7677
= <=>
2024-07-08 13:04:58 +0200

    Responsive ready!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,35 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-white text-black w-full h-screen">
-      <section className="w-[375px] bg-Green-200 px-1 py-2">
+    <main className="container bg-white text-black w-full h-screen mx-auto">
+      <section className="w-[375px] md:w-[1440px] bg-Green-200 px-1 py-2 content-center">
         <div className="bg-White rounded-2xl my-4 mx-3 py-3 px-4">
           <h1 className="text-3xl font-bold mt-5 mb-5">Contact Us</h1>
           <form action="">
-            <label className="mb-2  flex flex-col">
-              <span className="text-gray-700 font-[16px] mb-2">
-                First Name
-                <span className="ml-2 text-Green-600">*</span>
-              </span>
-              <input
-                type="text"
-                name="firstname"
-                className="text-black border border-gray-300 w-full px-3 py-2 mb-6 rounded-md disabled:border-Red"
-              />
-            </label>
-            <label className="mb-2 flex flex-col">
-              <span className="text-gray-700 font-[16px] mb-2">
-                Last Name
-                <span className="ml-2 text-Green-600">*</span>
-              </span>
-              <input
-                type="text"
-                name="lastname"
-                className="text-black border border-gray-300 w-full px-3 py-2 mb-6 rounded-md disabled:border-Red"
-              />
-            </label>
+            <div className="md:flex content-between">
+              <label className="mb-2 flex flex-col md:w-full sm:mr-2">
+                <span className="text-gray-700 font-[16px] mb-2">
+                  First Name
+                  <span className="ml-2 text-Green-600">*</span>
+                </span>
+                <input
+                  type="text"
+                  name="firstname"
+                  className="text-black border border-gray-300 w-full px-3 py-2 mb-6 rounded-md disabled:border-Red"
+                />
+              </label>
+              <label className="mb-2 flex flex-col md:w-full sm:ml-2">
+                <span className="text-gray-700 font-[16px] mb-2">
+                  Last Name
+                  <span className="ml-2 text-Green-600">*</span>
+                </span>
+                <input
+                  type="text"
+                  name="lastname"
+                  className="text-black border border-gray-300 w-full px-3 py-2 mb-6 rounded-md disabled:border-Red"
+                />
+              </label>
+            </div>
             <label className="mb-2 flex flex-col">
               <span className="text-gray-700 font-[16px] mb-2">
                 Email Address
@@ -44,31 +46,33 @@ export default function Home() {
               Query Type
               <span className="ml-2 text-Green-600">*</span>
             </p>
-            <div className="w-full h-12 border border-gray-300 rounded-lg px-5 py-3 mb-6">
-              <input
-                type="radio"
-                name="topping"
-                value="General Enquiry"
-                id="General_Enquiry"
-                // checked={selectedTopping === "Regular"}
-                // onChange={(e) => setSelectedTopping(e.target.value)}
-              />
-              <label className="ml-2" htmlFor="General_Enquiry">
-                General Enquiry
-              </label>
-            </div>
-            <div className="w-full h-12 border border-gray-300 rounded-lg px-5 py-3 mb-6">
-              <input
-                type="radio"
-                name="topping"
-                value="Support Request"
-                id="Support_Request"
-                // checked={selectedTopping === "Regular"}
-                // onChange={(e) => setSelectedTopping(e.target.value)}
-              />
-              <label className="ml-2" htmlFor="Support_Request">
-                Support Request
-              </label>
+            <div className="md:flex content-between">
+              <div className="w-full h-12 border border-gray-300 rounded-lg px-5 py-3 mb-6 sm:mr-2">
+                <input
+                  type="radio"
+                  name="topping"
+                  value="General Enquiry"
+                  id="General_Enquiry"
+                  // checked={selectedTopping === "Regular"}
+                  // onChange={(e) => setSelectedTopping(e.target.value)}
+                />
+                <label className="ml-2" htmlFor="General_Enquiry">
+                  General Enquiry
+                </label>
+              </div>
+              <div className="w-full h-12 border border-gray-300 rounded-lg px-5 py-3 mb-6 sm:ml-2">
+                <input
+                  type="radio"
+                  name="topping"
+                  value="Support Request"
+                  id="Support_Request"
+                  // checked={selectedTopping === "Regular"}
+                  // onChange={(e) => setSelectedTopping(e.target.value)}
+                />
+                <label className="ml-2" htmlFor="Support_Request">
+                  Support Request
+                </label>
+              </div>
             </div>
             <label className="mb-2 flex flex-col">
               <span className="text-gray-700 font-[16px] mb-2">
